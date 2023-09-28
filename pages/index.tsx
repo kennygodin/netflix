c311f07871d7b28df8f20423fb7ba02505c4606a
kennycodin <kenechukwuokoh30@gmail.com>
2023-09-28 04:54:10 +0100

    created serverAuth.ts, current.ts, fetcher.ts, /hooks, useCurrentUser.ts, adding swr, getServerSideProps, all to help protect routes and pages from users not logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,30 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+import { NextPageContext } from 'next';
+import { signOut, getSession } from 'next-auth/react';
 
-const inter = Inter({ subsets: ['latin'] });
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout!
+      </button>
     </>
   );
 }
