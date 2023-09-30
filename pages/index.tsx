6ce402a7433e03b15437e37728eb8c279d6bb0b8
kennycodin <kenechukwuokoh30@gmail.com>
2023-09-30 09:16:13 +0100

    created navbar.tsx and added it in index.ts, created NavbarItem.tsx and added it into Navbar for lg screens, created and added MobileMenu for sm screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
+import Navbar from '@/components/Navbar';
 import { NextPageContext } from 'next';
-import { signOut, getSession } from 'next-auth/react';
-
-import useCurrentUser from '@/hooks/useCurrentUser';
+import { getSession } from 'next-auth/react';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,14 +20,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout!
-      </button>
+      <Navbar />
     </>
   );
 }
