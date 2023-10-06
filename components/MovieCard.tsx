29fcb3da4f9203ada1c24fc1952daa9200648164
kennycodin <kenechukwuokoh30@gmail.com>
2023-10-06 19:01:34 +0100

    added the InfoModal component and fix

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { BsFillPlayFill } from 'react-icons/bs';
+import { BiChevronDown } from 'react-icons/bi';
 import FavouriteButton from './FavouriteBtn';
+import useInfoModal from '@/hooks/useInfoModal';
 
 interface MovieCardProps {
   data: Record<string, any>;
 }
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
+  const { openModal } = useInfoModal();
 
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
@@ -34,6 +37,15 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               />
             </div>
             <FavouriteButton movieId={data?.id} />
+            <div
+              onClick={() => openModal(data?.id)}
+              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
+            >
+              <BiChevronDown
+                className="text-white group-hover/item:text-neutral-300"
+                size={30}
+              />
+            </div>
           </div>
 
           <p className="text-green-400 font-semibold mt-4">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Billboard from '@/components/Billboard';
+import InfoModal from '@/components/InfoModal';
 import MovieList from '@/components/MovieList';
 import Navbar from '@/components/Navbar';
 import useFavourites from '@/hooks/useFavourites';
+import useInfoModal from '@/hooks/useInfoModal';
 import useMovieList from '@/hooks/useMovieLists';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
@@ -26,9 +28,11 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   const { data: movies = [] } = useMovieList();
   const { data: favourites = [] } = useFavourites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-10">
